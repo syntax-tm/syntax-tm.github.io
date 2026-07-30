67c74cc4186857305c6f6fbf1590b8bd1682e64e
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2026-07-30 16:33:37 -0500

    Updated menuBuilder.ts

--- src/services/menuBuilder.ts
+++ src/services/menuBuilder.ts
@@ -148,7 +148,7 @@ function buildSettingsCategory(): XmbCategory {
   viewSource.description = 'View this project on GitHub';
   viewSource.setActive();
 
-  const builds = new XmbItem("builds", "Source", icons.githubActions, 'https://github.com/syntax-tm/syntax-tm.github.io/actions');
+  const builds = new XmbItem("builds", "Builds", icons.githubActions, 'https://github.com/syntax-tm/syntax-tm.github.io/actions');
   builds.description = 'View latest builds on GitHub';
 
   const fork = new XmbItem("fork", "Fork", icons.codeFork, 'https://github.com/syntax-tm/syntax-tm.github.io/fork');
