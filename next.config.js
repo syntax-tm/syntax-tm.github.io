e6a11dda7e2f7a10647221a0a5dc6546d5d054b2
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2026-08-03 00:40:00 -0500

    Refactor code structure for improved readability and maintainability

--- next.config.js
+++ next.config.js
@@ -2,28 +2,15 @@ import version from './package.json' with { type: 'json' };
 import { fileURLToPath } from "url";
 import path from 'path';
 import WebpackShellPluginNext from "webpack-shell-plugin-next";
-//import type { NextConfig } from 'next';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const dependencies = Object.keys(version.dependencies).join('\n');
 
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig}*/
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
   output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
   basePath: "",
-
   compiler: {
     styledComponents: true,
   },
@@ -33,12 +20,6 @@ const nextConfig = {
   sassOptions: {
     implementation: 'sass-embedded',
   },
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
   images: {
     unoptimized: true,
   },
@@ -46,7 +27,6 @@ const nextConfig = {
     // TODO: add missing Suspense and remove this suppression
     // missingSuspenseWithCSRBailout: false,
     viewTransition: true,
-    useTypeScriptCli: true,
   },
   env: {
     name: version.name,
@@ -69,6 +49,7 @@ const nextConfig = {
       },
     },
   },
+  /** @param {import('webpack').Configuration} config */
   webpack: (config, { dev, isServer }) => {
     if (isServer && !dev) {
       config.plugins.push(
